177ddc3d7e6fd1b6eb8397deb6d6c684ce7ba298
lrene4872@naver.com <lrene4872@naver.com>
2025-08-13 10:07:09 +0900

    feat: requestDTO 유효성 검사 추가

--- src/main/java/com/campinglog/campinglogbackserver/board/dto/request/RequestAddComment.java
+++ src/main/java/com/campinglog/campinglogbackserver/board/dto/request/RequestAddComment.java
@@ -1,5 +1,7 @@
 package com.campinglog.campinglogbackserver.board.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +12,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class RequestAddComment {
+
+    @NotBlank(message = "게시글 ID는 필수입니다.")
     private String boardId;
+
+    @NotBlank(message = "댓글 내용은 필수입니다.")
+    @Size(min = 1, max = 1000, message = "댓글은 1자 이상 1000자 이하여야 합니다.")
     private String content;
+
+    @NotBlank(message = "닉네임은 필수입니다.")
     private String nickname;
 
 }
