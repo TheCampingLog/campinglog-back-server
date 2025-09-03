1de6fd2e8bf3f40fbaf526650e3e306637425104
goldtwo1989 <ttpd19891213@gmail.com>
2025-09-03 19:20:15 +0900

    fix: 회원랭킹 조회 revert 해결

--- src/main/java/com/campinglog/campinglogbackserver/CampinglogBackServerApplication.java
+++ src/main/java/com/campinglog/campinglogbackserver/CampinglogBackServerApplication.java
@@ -2,7 +2,9 @@ package com.campinglog.campinglogbackserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication()
 public class CampinglogBackServerApplication {
 
